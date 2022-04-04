0dd23c11f608da5dc35598d2f9549b8a52ad10aa
Jeonghwan-Yoo <dwgbjdhks2@gmail.com>
2022-04-04 11:17:51 +0900

    Add constructor injection

--- spring-demo-annotation/src/com/luv2code/springdemo/PingpongCoach.java
+++ spring-demo-annotation/src/com/luv2code/springdemo/PingpongCoach.java
@@ -1,13 +1,25 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PingpongCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public PingpongCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your pingpong drop shot";
 	}
 
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
 }
