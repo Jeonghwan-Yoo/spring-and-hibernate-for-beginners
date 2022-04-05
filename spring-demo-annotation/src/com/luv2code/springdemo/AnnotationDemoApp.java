e13d79f9ba42a5def563b5c8d97ced724cb6f85f
Jeonghwan-Yoo <dwgbjdhks2@gmail.com>
2022-04-05 09:22:39 +0900

    Add how to inject properties file using annotations

--- spring-demo-annotation/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotation/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -10,13 +10,19 @@ public class AnnotationDemoApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// get the bean from spring container
-		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		// Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		SwimCoach swimCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		// call a method on the bean
-		System.out.println(theCoach.getDailyWorkout());
+		// System.out.println(theCoach.getDailyWorkout());
+		System.out.println(swimCoach.getDailyWorkout());
 		
 		// call method to get the daily fortune
-		System.out.println(theCoach.getDailyFortune());
+		// System.out.println(theCoach.getDailyFortune());
+		System.out.println(swimCoach.getDailyFortune());
+		
+		System.out.println(swimCoach.getEmail());
+		System.out.println(swimCoach.getTeam());
 		
 		// close the context
 		context.close();
