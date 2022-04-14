a94d17697c4d67150a369a80f9c50f4d2feb0fc2
Jeonghwan-Yoo <dwgbjdhks2@gmail.com>
2022-04-14 10:30:54 +0900

    Add radiobuttons

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -11,6 +11,10 @@ public class Student {
 	
 	private LinkedHashMap<String, String> countryOptions;
 	
+	private String favoriteLanguage;
+	
+	private LinkedHashMap<String, String> favoriteLanguageOptions;
+	
 	public Student() {
 		// populate country options: used ISO country code
 		countryOptions = new LinkedHashMap<String, String>();
@@ -20,6 +24,13 @@ public class Student {
 		countryOptions.put("DE", "Germany");
 		countryOptions.put("IN", "India");
 		countryOptions.put("US", "United States of America");
+		
+		favoriteLanguageOptions = new LinkedHashMap<String, String>();
+		
+		favoriteLanguageOptions.put("Java", "Java");
+		favoriteLanguageOptions.put("C#", "C#");
+		favoriteLanguageOptions.put("PHP", "PHP");
+		favoriteLanguageOptions.put("Ruby", "Ruby");
 	}
 	
 	public String getFirstName() {
@@ -50,4 +61,16 @@ public class Student {
 		return countryOptions;
 	}
 
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public LinkedHashMap<String, String> getFavoriteLanguageOptions() {
+		return favoriteLanguageOptions;
+	}
+	
 }
